199c8b5784b4711d8c651f01e3dae2f456eb3488
Karim Jordan <jordan.kaerim@gmail.com>
2021-06-18 23:28:05 +0300

    update errors again

--- src/lib/dictionary/en.js
+++ src/lib/dictionary/en.js
@@ -3,13 +3,6 @@ export default {
 		ERROR: 'An error occurred.',
 		OFFLINE: 'You are not connected to the Internet.',
 
-		500: '500: An error has occured.',
-		400: '400: Invalid Input',
-		401: '401: You are not logged in.',
-		403: '403: You are not authorized to perform this action.',
-		404: '404: The requested ressource could not be found.',
-
-		DB_ERROR: 'An error has occured.',
 		BAD_REQUEST: 'Invalid Input',
 		UNAUTHORIZED: 'You are not logged in.',
 		FORBIDDEN: 'You are not authorized to perform this action.',
